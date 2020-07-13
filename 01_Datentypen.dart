288f767c7ce6f480ff6dc4a0a92e73e855ca70f9
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-07-13 14:43:27 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,14 @@
+void main() {
+  int i = 4;
+  int a = 5;
+  print(i);
+  print(a);
+  bool entscheide = true;
+  bool willst_du_das = false;
+  print(entscheide);
+  print(willst_du_das);
+  String e = "hi";
+  String z = "wo?";
+  print(e);
+  print(z);
+}

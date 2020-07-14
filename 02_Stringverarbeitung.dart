0dc6c4bdf0ecbe58c007e0b3c6d96a97a6ce1523
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-07-14 10:33:00 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,11 @@
+main()
+{
+String meinName = "Marc";
+
+  print("Hallo ich bin $meinName");
+  double doub =3.75;
+  String cast = doub.toString();
+  print(cast);
+  int laenge = meinName.length;
+  print(laenge);
+}
